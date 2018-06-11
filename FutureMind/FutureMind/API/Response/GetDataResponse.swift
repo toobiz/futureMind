494dfb10e9f8f2c361e57e06965e34a825f9e088
Michał Tubis <michal.tubis@ynd.co>
2018-06-11 18:49:44 +0200

    * Parse description without url link * Add link property to DataResponse

--- FutureMind/FutureMind/API/Response/GetDataResponse.swift
+++ FutureMind/FutureMind/API/Response/GetDataResponse.swift
@@ -22,6 +22,7 @@ class Item: ImmutableMappable {
     
     let title: String?
     let description: String?
+    let link: String?
     let orderId: Int?
     let modificationDate: String?
     let imageUrl: String?
@@ -29,10 +30,60 @@ class Item: ImmutableMappable {
     
     required init(map: Map) throws {
         title = try? map.value("title")
-        description = try? map.value("description")
+        description = try? map.value("description", using: ImmutableMappableTransformation.removeUrl())
+        link = try? map.value("description", using: ImmutableMappableTransformation.extractUrl())
         orderId = try? map.value("orderId")
         modificationDate = try? map.value("modificationDate")
         imageUrl = try? map.value("image_url")
         image = try? map.value("image")
+    }
+    
+    struct ImmutableMappableTransformation {
+        
+        static func removeUrl() -> TransformOf<String, Any> {
+            return TransformOf<String, Any>(fromJSON: { (value: Any?) -> String? in
+                if let value = value as? String {
+                    var string = String()
+                    if let index = value.range(of: "http")?.lowerBound {
+                        let substring = value[..<index]
+                        string = String(substring).condenseWhitespace()
+                    }
+                    return string
+                }
+                return nil
+            }, toJSON: { (value: String?) -> String? in
+                if let value = value {
+                    return value
+                }
+                return nil
+            })
+        }
+        
+        static func extractUrl() -> TransformOf<String, Any> {
+            return TransformOf<String, Any>(fromJSON: { (value: Any?) -> String? in
+                if let value = value as? String {
+                    var string = String()
+                    if let index = value.range(of: "http")?.lowerBound {
+                        let substring = value[index..<value.endIndex]
+                        string = String(substring)
+                        print(string)
+                    }
+                    return string
+                }
+                return nil
+            }, toJSON: { (value: String?) -> String? in
+                if let value = value {
+                    return value
+                }
+                return nil
+            })
+        }
+    }
+}
+
+extension String {
+    func condenseWhitespace() -> String {
+        let components = self.components(separatedBy: NSCharacterSet.whitespacesAndNewlines)
+        return components.filter { !$0.isEmpty }.joined(separator: " ")
     }
 }
